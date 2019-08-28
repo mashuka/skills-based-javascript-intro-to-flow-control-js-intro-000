a558e4ac88fabab4d8891834af43303a724eee54
active-collaborator-0000 <dianamayer0@gmail.com>
2019-08-28 09:13:59 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -14,7 +14,7 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-  return age ? "You are a teenager" : "You are not a teenager"
+  return age ? "You are a teenager" : "You are not a teenager";
 }
 
 function switchAge(age) {
